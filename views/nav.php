da7dac6e4fed170bea825bc0811d58bb4c0dc3fa
Marwan Said <63176373+1iPluto@users.noreply.github.com>
2023-03-22 05:12:47 +0200

    Add files via upload

---
+++ views/nav.php
@@ -0,0 +1,13 @@
+<?php if(!defined("SITE_NAME")) die(); ?> 
+<nav class="top-nav">
+    <div class="container">
+        <a class="top-nav-logo" href="<?= getLink("/") ?>">
+            <img src="<?= SITE_LOGO ?>" alt="" />
+        </a>
+
+        <div class="top-nav-links">
+            <a href="<?= getLink("/") ?>">Paste</a>
+        </div>
+    </div>
+</nav>
+<div class="top-nav-holder"></div>
